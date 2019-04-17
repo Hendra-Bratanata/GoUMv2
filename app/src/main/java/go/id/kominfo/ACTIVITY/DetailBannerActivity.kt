12280e6c4a90cfb603286b8ec20388e5b12cd84f
ardiyansyahh20 <ardiyansyahh20@gmail.com>
2019-04-17 23:08:55 +0700

    sales order terbaru

---
+++ app/src/main/java/go/id/kominfo/ACTIVITY/DetailBannerActivity.kt
@@ -0,0 +1,13 @@
+package go.id.kominfo.ACTIVITY
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import go.id.kominfo.R
+
+class DetailBannerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_banner)
+    }
+}
